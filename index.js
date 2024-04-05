d09e7f57cba697635068d60469fd5ec2d84bc22f
Pranjal <122043570+Itssmepj@users.noreply.github.com>
2024-04-05 13:55:02 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function toggleMenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const profilePic = document.getElementById("profile");
+  
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
-}
+  
+    if (menu.classList.contains("open")) {
+      profilePic.style.marginTop = "170px";
+    } else {
+      profilePic.style.marginTop = "0";
+    }
+  }
